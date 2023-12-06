b8e959a94ac88efff575e92481c32bd651171991
Salvador-GA <81882609+Salvador-GA@users.noreply.github.com>
2023-12-06 11:33:37 -0600

    Add files via upload

---
+++ EjClaseDerivada/Esfera.java
@@ -0,0 +1,57 @@
+package EjClaseDerivada;
+
+/**
+ * Clase que representa una Esfera a partir del radio
+ * @author Salvador Gonzalez Arellano
+ * @version 1.0
+ */
+public class Esfera {
+    private double radio;
+
+    /**
+     * Constructor de la clase para inicializar el radio
+     * @param radio el valor del radio
+     */
+    protected Esfera(double radio) {
+        this.radio = radio;
+    }
+
+    /**
+     * Constructor de la clase para inicializar el radio a 0
+     */
+    protected Esfera() {
+        this.radio = 0.0;
+    }
+
+    /**
+     * Funcion para devolver el valor del radio
+     * @return el valor del radio
+     */
+    protected double getRadio() {
+        return radio;
+    }
+
+    /**
+     * Funcion para asignar un valor al radio
+     * @param radio valor del radio
+     */
+    protected void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    /**
+     * Funcion para calcular el radio de la esfera
+     * @return superficie de la esfera
+     */
+    protected double superficie() {
+        return 4 * Math.PI * Math.pow(radio, 2);
+    }
+
+    /**
+     * Funcion que devuelve la informacion de la esfera en un String
+     * @return Informacion de la esfera en un String
+     */
+    public String toString() {
+        return "Radio = " + radio;
+    }
+}
---
+++ EjClaseDerivada/Principal.java
@@ -0,0 +1,23 @@
+package EjClaseDerivada;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Esfera miesfera1 = new Esfera();
+        Esfera miesfera2;
+        Esfera miesfera3 = new Esfera(2.0);
+
+        miesfera1.setRadio(1.0);
+        miesfera2 = miesfera1;
+        System.out.println("Las referencias miesfera1 y miesfera2 " + (miesfera1 == miesfera2 ? "si" : "no")
+                + " apuntan al mismo objeto");
+        System.out.println("Las referencias miesfera3 y miesfera1 " + (miesfera3 == miesfera1 ? "si" : "no")
+                + " apuntan al mismo objeto");
+        Planeta tierra = new Planeta(6378, 1);
+        double superficie = tierra.superficie();
+        System.out.println("La superficie de la tierra es: " + superficie + " km y cuenta con "
+                + tierra.getNumSatelites() + " satelite");
+        System.out.println("string de tierra: " + tierra.toString());
+        System.out.println("string de miesfera3: " + miesfera3.toString());
+    }
+}
