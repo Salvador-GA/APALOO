353a4a79308af330a79c4b9bd3f82144024b8daf
Salvador-GA <81882609+Salvador-GA@users.noreply.github.com>
2023-12-06 11:27:53 -0600

    Add files via upload

---
+++ EjemploPunto/Punto.java
@@ -0,0 +1,60 @@
+package EjemploPunto;
+/**
+ * Clase Punto para almacenar las coordenadas cartesianas x, y
+ * @author Salvador Gonzalez Arellano
+ * @version 1.0
+ */
+class Punto {
+    private int x, y;
+    
+    /**
+     * Constructor de la calse para inicializar las coordenadas x, y
+     * @param x valor de la coordenada x
+     * @param y valor de la coordenada y
+     */
+    public Punto(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Constructor de la clase para inicializar las coordenadas a 0
+     */
+    public Punto() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    /**
+     * Función para devolver el valor de la coordenada x
+     * @return valor de la coordenada x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Función para asignar un valor a la coordenada x
+     * @param x valor para la coordenada x
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * Función para devolver el valor de la coordenada y
+     * @return valor de la coordenada y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Función para asignar un valor a la coordenada x
+     * @param y valor para la coordenada y
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+}
