56e8e1a81d3c2b0cbfe97c34c820091da04e062b
Salvador-GA <81882609+Salvador-GA@users.noreply.github.com>
2024-10-28 10:12:53 -0600

    Update Principal.java

--- 1.5 EjPolimorfismo/Principal.java
+++ 1.5 EjPolimorfismo/Principal.java
@@ -1,5 +1,5 @@
 public class Principal {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Animal[] animales = new Animal[6];
         animales[0] = new Perro();
         animales[1] = new Gato();
